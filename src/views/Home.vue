0ed4514ee27580eefd39d8f906f91915e8be479e
uday lakkoju <uday.lakkoju@wipro.com>
2021-04-18 12:40:36 +0530

    beer information table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,49 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import "vue-good-table/dist/vue-good-table.css";
+import { VueGoodTable } from "vue-good-table";
+import beersData from "../assets/beers.json";
+import About from "./About.vue";
+const axios = require('axios');
+export default {
+  name: "Home",
+  data() {
+    return {
+      columns: [
+        {
+          label: "Name",
+          field: "name",
+        },
+        {
+          label: "Brewery",
+          field: "brewery_id",
+          type: "number",
+          sortable: false,
+        },
+        {
+          label: "Last Modified",
+          field: "last_mod",
+          sortable: false,
+        },
+      ],
+      rows:beersData.data
+    };
+  },
+  created() {
+  // GET request using axios with error handling
+  let location = '../assets/beers.json'
+  axios.get(location)
+    .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
 
+  
 export default {
   name: 'Home',
   components: {
